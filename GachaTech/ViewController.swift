67753d1d3498c96335afa56ab0c39e41b959d43c
Riko Yasuda <rikomintea1827@gmail.com>
2021-05-14 14:14:40 +0900

    Initial Commit

---
+++ GachaTech/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GachaTech
+//
+//  Created by スマート・ナビ on 2021/05/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
